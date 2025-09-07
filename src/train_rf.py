a53a765a6d0bdc2e5f1765dfb3122bc7fb92cea8
nirajain21 <s223144439@deakin.edu.au>
2025-09-07 20:24:43 +1000

    Update train_rf.py

--- src/train_rf.py
+++ src/train_rf.py
@@ -1,3 +1,4 @@
+#Focused training for Random Forest only.
 import os, pandas as pd, numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
